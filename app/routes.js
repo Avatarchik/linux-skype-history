23aa1887e295bf8ac83431c7c00b2ecd4db33b30
Markus Mulkahainen <markus.mulkahainen@hotmail.com>
2016-06-01 19:44:57 +0300

    decodes html special characters

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,7 @@
 var jade = require("jade");
 var messageRepository = require("./messageRepository.js");
+var Entities = require('html-entities').XmlEntities;
+var entities = new Entities();
 
 module.exports = function(app, skypeUsers) {
 
@@ -14,6 +16,7 @@ module.exports = function(app, skypeUsers) {
         if (err) {
           console.log(err.message);
         }
+        messages.filter(message => message.msg !== null).forEach(message => message.msg = entities.decode(message.msg));
         var html = jade.renderFile("app/views/index.jade", { users: skypeUsers.getUsers(), selectedUser: selectedUser, contacts: contacts, messages: messages });
         res.send(html);
       });
